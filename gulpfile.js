46998972cba1b31db7996d8869e3effef2ee3bb9
deadie <deadie@yandex.ru>
2016-12-25 01:06:31 +0300

    2016.12.25

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,55 @@
 'use strict';
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    prefixer = require('gulp-autoprefixer'),
-    cleancss = require('gulp-clean-css'),
-    sourcemaps = require('gulp-sourcemaps'),
-    rename = require('gulp-rename'),
-    plumber = require('gulp-plumber'),
-    watch = require('gulp-watch');
+  sass = require('gulp-sass'),
+  prefixer = require('gulp-autoprefixer'),
+  cleancss = require('gulp-clean-css'),
+  sourcemaps = require('gulp-sourcemaps'),
+  rename = require('gulp-rename'),
+  plumber = require('gulp-plumber'),
+  watch = require('gulp-watch');
 
 var path = {
-    build: {
-        css: 'css'
-    },
-    src: {
-        css: 'sass/sevendie.scss'
-    },
-    watch: {
-        css: 'sass/**/*.*'
-    },
-    clean: './css'
+  build: {
+    css: 'css'
+  },
+  src: {
+    css: 'sass/sevendie.scss'
+  },
+  watch: {
+    css: 'sass/**/*.*'
+  },
+  clean: './css'
 };
 
 gulp.task('cssOwn:build', function () {
-    gulp.src(path.src.css)
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            compress: true,
-            'include css': true
-        }))
-        .pipe(prefixer({
-            browsers: ['last 3 version', "> 1%", "ie 8", "ie 7"]
-        }))
-        .pipe(cleancss({compatibility: 'ie8'}))
-        .pipe(sourcemaps.write('.'))
-        //.pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(path.build.css))
+  gulp.src(path.src.css)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      compress: true,
+      'include css': true
+    }))
+    .pipe(prefixer({
+      browsers: ['last 3 version', "> 1%", "ie 8", "ie 7"]
+    }))
+    .pipe(cleancss({compatibility: 'ie8'}))
+    .pipe(sourcemaps.write('.'))
+    //.pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(path.build.css))
 });
 
 gulp.task('css:build', [
-    'cssOwn:build'
+  'cssOwn:build'
 ]);
 
 gulp.task('build', [
-    'css:build'
+  'css:build'
 ]);
 
-gulp.task('watch', function(){
-    watch([path.watch.css], function(event, cb) {
-        gulp.start('css:build');
-    });
+gulp.task('watch', function () {
+  watch([path.watch.css], function (event, cb) {
+    gulp.start('css:build');
+  });
 });
 
 gulp.task('default', ['build', 'watch']);
---
+++ js/sevendie.js
@@ -0,0 +1,11 @@
+(function ($, Drupal, window, document, undefined) {
+  Drupal.behaviors.my_custom_behavior = {
+    attach: function(context, settings) {
+//---------------------------
+
+
+
+//---------------------------
+    }
+  };
+})(jQuery, Drupal, this, this.document);
